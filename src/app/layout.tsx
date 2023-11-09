01276cbe73e72fc2afca26553f17ebeea2c3bf66
jamesng-minhkhoi <136988400+jamesng-minhkhoi@users.noreply.github.com>
2023-11-10 00:31:22 +0700

    cursor custom & layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './ui/header'
+import Cursor from './ui/cursor'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       
         <Header></Header>
         {children}
+
         {/* <Footer></Footer> */}
       </body>
     </html>
